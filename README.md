5f3321411a70b8852f8b8114f6cb21e0d60154e3
Ivy Wu <ivycong60@gmail.com>
2020-01-05 16:06:26 -0500

    First Commit by Ivy

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Data-Modelling-with-Postgres
-Project in Data Engineering course
+## How to run whole project
+1. Run 'python creat_tables.py'
+2. Run  'test.ipynb'
+3. restart 'test.ipynb'
+4. Run 'etl.ipynb'
+5. restart 'etl.ipynb'
+6. Run 'python creat_tables.py' again and Run'etl.py'
+
+
+## Project Analysis
+1. Discuss the purpose of this database in the context of the startup, Sparkify, and their analytical goals.
+The purpose of this database is to extract userful information from log data and song data for analyzing users who use music streaming app, such as what kinds of song, whether the user is using paid or free mode, where is the user located, what media they use to play songs from log data and song data. Their analytical goals are what kinds of song are most frequently played by the users.
+
+2. State and justify your database schema design and ETL pipeline.
+I created a star schema by including one fact table and  four dimension tables.Songplays table is a fact table to store song records of users use music streaming app.Four dimension tables -users, songs, artists and time, are used to store details of song records and users' information.
+ELT pipeline is important step in this prokect. Becasue I have to extract data from log datasets and song datasets to build each table. However, some data formats are neccessary to transformation duing pipeling data, such as wh
+3. [Optional] Provide example queries and results for song play analysis.
+![Alt text](https://github.com/Waichung1015/Data-Modelling-with-Postgres/blob/master/image.png)
+We can find that one song from songs table are listened by user, other songs listened by users can't find from song table.
---
+++ sql_queries.py
@@ -0,0 +1,58 @@
+# DROP TABLES
+
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
+
+# CREATE TABLES
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays(songplay_id SERIAL PRIMARY KEY, start_time numeric NOT NULL, user_id int NOT NULL, level varchar , song_id varchar , artist_id varchar, session_id int NOT NULL, location varchar, user_agent varchar);
+""")
+# create dimension tables
+# user in app
+# can't duplicate add the user with same first name and last name
+
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users(user_id int UNIQUE NOT NULL PRIMARY KEY, first_name varchar, last_name varchar, gender varchar, level varchar);
+""")
+# song in music db
+# can't duplicate add same title song
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs(song_id varchar UNIQUE NOT NULL PRIMARY KEY, title varchar, artist_id varchar NOT NULL, year int, duration numeric);
+""")
+
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists(artist_id varchar UNIQUE NOT NULL PRIMARY KEY, name varchar UNIQUE, location varchar, latitude numeric, longitude numeric);
+""")
+#timestamps of records in songplays broken down into specific unit
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time(start_time numeric UNIQUE PRIMARY KEY, hour int, day int, week int, month int, year int, weekday varchar);
+""")
+
+
+# INSERT RECORDS
+
+songplay_table_insert = ("""INSERT INTO songplays(start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES (%s, %s,%s,%s,%s,%s,%s,%s)
+""")
+
+user_table_insert = ("""INSERT INTO users(user_id,first_name,last_name,gender,level) VALUES (%s, %s, %s, %s,%s) ON CONFLICT (user_id) DO UPDATE SET level = users.level
+""")
+
+song_table_insert = ("""INSERT INTO songs(song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s,%s) ON CONFLICT(song_id) DO NOTHING
+""")
+
+artist_table_insert = ("""INSERT INTO artists(artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s,%s) ON CONFLICT(artist_id) DO NOTHING
+""")
+
+
+time_table_insert = ("""INSERT INTO time(start_time,hour,day,week, month, year, weekday) VALUES (%s, %s, %s, %s,%s,%s,%s) ON CONFLICT(start_time) DO NOTHING
+""")
+
+# FIND SONGS
+
+song_select = (""" SELECT song_id,artists.artist_id FROM songs,    
+                  artists WHERE (songs.artist_id = artists.artist_id AND songs.title = %s 
+                  AND artists.name = %s AND songs.duration = %s)
+""")
+
+# QUERY LISTS
+
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
